27642877d0f8517cc1e75214be8be247eed19d8e
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-19 17:57:55 +0800

    feat: db migrations helper

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,7 @@ import { schema } from "@/db/schema";
 import { AUTH_TOKEN, DB_URL } from "@/lib/env";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
+import { migrate } from "drizzle-orm/libsql/migrator";
 
 const dbClient = createClient({
     url: DB_URL,
@@ -11,3 +12,9 @@ const dbClient = createClient({
 const db = drizzle(dbClient, { schema });
 
 export default db;
+
+export const setupDbWithMigrations = async (migrationsFolder: string) => {
+    if (DB_URL !== ":memory:") return;
+    console.log("Performing migrations for an in-memory database.");
+    await migrate(db, { migrationsFolder });
+};
