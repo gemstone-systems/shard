6b0474dfe9145ba4b378f4e0857431e3b0050ffb
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-19 23:52:13 +0800

    feat: db schema helpers

--- src/db/schema/messages.ts
+++ src/db/schema/messages.ts
@@ -1,6 +1,8 @@
 import { sql } from "drizzle-orm";
 import { integer, text, index } from "drizzle-orm/sqlite-core";
 import { sqliteTable } from "drizzle-orm/sqlite-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
 // NOTE:: for an initial mvp, we are supporting only sqlite and storing all the messages in a single table.
 // for an actual production release, we will likely be storing messages in separate tables or separate sqlite files (depending on config).
@@ -22,3 +24,10 @@ export const messagesTable = sqliteTable(
         index("messages_author_idx").on(table.authorDid),
     ],
 );
+
+export const messagesSelectSchema = createSelectSchema(messagesTable);
+export const messageSelectSchemaArray = z.array(messagesSelectSchema);
+export const messagesInsertSchema = createInsertSchema(messagesTable);
+
+export type ShardMessageSelect = z.infer<typeof messagesSelectSchema>;
+export type ShardMessageInsert = z.infer<typeof messagesInsertSchema>;
