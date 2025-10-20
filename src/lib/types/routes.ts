981f84e90b4d7b41b82974a3b09e086669a8b509
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-20 21:37:05 +0800

    refactor: http handler signature

--- src/lib/types/routes.ts
+++ src/lib/types/routes.ts
@@ -2,8 +2,8 @@ import type { FastifyReply, FastifyRequest } from "fastify";
 import type { WebSocket } from "ws";
 
 export type RouteHandler = (
-    req: FastifyRequest | undefined,
-    reply: FastifyReply | undefined,
+    req: FastifyRequest,
+    reply: FastifyReply,
 ) => Response | Promise<Response>;
 
 export type Method = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
