f2bce17bd105170ca7128312838a4c80a7daf170
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-20 12:44:30 +0800

    feat: temporary lexicon type

---
+++ src/lib/types/gmstn.ts
@@ -0,0 +1,10 @@
+import type { ComAtprotoRepoStrongRef } from "@atcute/atproto";
+
+export interface SystemsGmstnDevelopmentChannel {
+    name: string;
+    $type: string;
+    topic: string;
+    storeAt: ComAtprotoRepoStrongRef.Main;
+    routeThrough: ComAtprotoRepoStrongRef.Main;
+    createdAt: string;
+}
