3282e7a643ecb9b8c3ff77dfd050f4f6e908ddee
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-10-23 02:53:42 +0800

    refactor: apply port to did web

--- src/lib/utils/verifyJwt.ts
+++ src/lib/utils/verifyJwt.ts
@@ -1,11 +1,14 @@
-import { SERVICE_DID } from "@/lib/env";
+import { SERVER_PORT, SERVICE_DID } from "@/lib/env";
 import { didDocResolver } from "@/lib/utils/atproto";
 import { ServiceJwtVerifier } from "@atcute/xrpc-server/auth";
 
 export const verifyServiceJwt = async (jwt: string) => {
+    const serviceDid = SERVICE_DID.startsWith("did:web:localhost")
+        ? (`${SERVICE_DID}%3A${SERVER_PORT.toString()}` as `did:${string}:${string}`)
+        : SERVICE_DID;
     const verifier = new ServiceJwtVerifier({
         resolver: didDocResolver,
-        serviceDid: SERVICE_DID,
+        serviceDid,
     });
     return await verifier.verify(jwt);
 };
