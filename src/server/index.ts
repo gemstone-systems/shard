528c8c4a623ad4321162e1690d518d24a2d50940
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-07 21:42:51 +0800

    feat: cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,14 @@
 import websocket from "@fastify/websocket";
+import cors from "@fastify/cors";
 import Fastify from "fastify";
 
 export const setupServer = async () => {
     const fastify = Fastify({
         logger: true,
+    });
+
+    await fastify.register(cors, {
+        origin: true,
     });
 
     await fastify.register(websocket);
